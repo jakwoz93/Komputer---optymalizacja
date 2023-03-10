5e5f5f0a322be7b53cd52f54e63471c639e6d9c7
jakwoz93 <127531485+jakwoz93@users.noreply.github.com>
2023-03-10 17:00:37 +0100

    Add files via upload

---
+++ Dokumenty - organizacja.bat
@@ -0,0 +1,6 @@
+cd /d %userprofile%/desktop
+md PDF WORD EXCEL
+move *.docx %userprofile%\Desktop\WORD
+move *.doc %userprofile%\Desktop\WORD
+move *.pdf %userprofile%\Desktop\PDF
+move *.xls %userprofile%\Desktop\EXCEL
---
+++ Kopiuj drukarkę z innego komputera.ps1
@@ -0,0 +1,11 @@
+﻿write-host
+$starykomputer = $(Read-Host "IP/hostname dawcy")
+get-printerport -computername $starykomputer 
+write-host
+$drukarka = $(Read-Host "Podaj IP lub nazwę drukarki")
+write-host
+$nowykomputer = $(Read-Host "IP/hostname odbiorcy")
+write-host
+$Nazwadrukarki = $(Read-Host "Podaj nazwę drukarki")
+add-printerport -computername $nowykomputer -Name $drukarka -PrinterHostAddress $drukarka
+Add-Printer -computername $nowykomputer -Name "$Nazwadrukarki" -DriverName "Microsoft PCL6 Class Driver" -PortName "$drukarka"
